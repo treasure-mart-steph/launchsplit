434cc92481ce49d96cd286c3eaab10e5adb36916
Ryan <yardsticks@gmail.com>
2023-03-14 11:33:34 -0700

    fix windows support

--- install.bat
+++ install.bat
@@ -4,10 +4,12 @@ rem Create a new virtual environment
 python -m venv venv
 
 rem Activate the virtual environment
-call .\venv\bin\activate.ps1
+.\venv\Scripts\activate
 
 rem Install required packages
 pip install -r requirements.txt
 
 rem Deactivate the virtual environment
 deactivate
+
+timeout /t 10
