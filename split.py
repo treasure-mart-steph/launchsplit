e2cdbfd9260f9db9cb0f7a8f19b1e443ac2e05f0
Ryan <ryan@treasuremart.net>
2023-03-14 09:58:01 -0700

    it works now

--- split.py
+++ split.py
@@ -1,17 +1,15 @@
 import pandas as pd
 
 # Set the input filename
-filename = "launch.csv"
+filename = "input/launch.csv"
 
-# Read in the CSV file
-df = pd.read_csv(filename)
+# Read in the CSV file, skipping the first row
+df = pd.read_csv(filename, skiprows=1)
 
-# Split the data into three separate data frames based on the sections you described
-section1 = df.iloc[:, :10] # replace 10 with the number of columns in section 1
-section2 = df.iloc[:, 10:20] # replace 10 and 20 with the start and end column indices for section 2
-section3 = df.iloc[:, 20:] # replace 20 with the start column index for section 3
+# Split the data into two separate data frames
+section1 = df.iloc[:, 63:115] # columns 64 to 115
+section2 = df.iloc[:, 115:143] # columns 116 to 143
 
 # Save each data frame as a separate CSV file
-section1.to_csv('section1.csv', index=False)
-section2.to_csv('section2.csv', index=False)
-section3.to_csv('section3.csv', index=False)
+section1.to_csv('output/the_launches.csv', index=False)
+section2.to_csv('output/data_builder.csv', index=False)
